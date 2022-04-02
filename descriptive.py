dfc031c49e3a40c892c0fad67c378c7e92abc9c2
Ameya Shere <ameya.shere@publicissapient.com>
2022-04-02 19:57:18 -0400

    add replies logging

--- descriptive.py
+++ descriptive.py
@@ -1,5 +1,5 @@
 import json
-from preprocessing.preprocessing import extract_tweet_ids, extract_text_data
+from preprocessing.preprocessing import Preprocessor
 
 # Load API keys from file
 api_keys = dict()
@@ -8,12 +8,13 @@ with open('api-keys/api_keys_ameya.txt', 'r') as f:
     api_keys = json.loads(f.read())
 
 # TODO: Edit preprocessing to maximize info returned in one request
+proc = Preprocessor(logging=False)
 
 # Extract tweet id's into data/processed folder
-#extract_tweet_ids()
+#proc.extract_tweet_ids()
 
 # Get text from API
-#extract_text_data(api_keys, 30)
+#proc.extract_text_data(api_keys, 30)
 
 # TODO: Get metrics from API
 
